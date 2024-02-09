89fd333bca438bd848bc6aeaf8ec607856b3b661
Narendran S <122251349+SNarendranS@users.noreply.github.com>
2024-02-09 10:24:17 +0530

    Update newsService.js

--- news-portal-frontend/src/services/newsService.js
+++ news-portal-frontend/src/services/newsService.js
@@ -2,26 +2,26 @@ import axios from 'axios'
 
 const createNews=(news)=>{
     const token=sessionStorage.getItem('token')
-    return axios.post('http://localhost:8080/news',news,{headers:{'Authorization':'Bearer '+JSON.parse(token)}})
+    return axios.post('https://chronical-backend.onrender.com/news',news,{headers:{'Authorization':'Bearer '+JSON.parse(token)}})
 
 }
 
 const getAllNews=()=>{
-    return axios.get('http://localhost:8080/news/all')
+    return axios.get('https://chronical-backend.onrender.com/news/all')
 }
 const getNews=(genre)=>{
-    return axios.get(`http://localhost:8080/news/genre/${genre}`)
+    return axios.get(`https://chronical-backend.onrender.com/news/genre/${genre}`)
 }
 const myNews=()=>{
     const token=sessionStorage.getItem('token')
-    return axios.get(`http://localhost:8080/news`,{headers:{'Authorization':'Bearer '+JSON.parse(token)}})
+    return axios.get(`https://chronical-backend.onrender.com/news`,{headers:{'Authorization':'Bearer '+JSON.parse(token)}})
 
 }
 const latestNews=()=>{
-    return axios.get('http://localhost:8080/news/latest')
+    return axios.get('https://chronical-backend.onrender.com/news/latest')
 }
 const searchNews=(tag)=>{
-    return axios.get(`http://localhost:8080/news/search/${tag}`)
+    return axios.get(`https://chronical-backend.onrender.com/news/search/${tag}`)
 }
 const NewsService={
     createNews,getAllNews,getNews,myNews,latestNews,searchNews
