902623e25bb9895f1ba968cc758514d9e576e2d9
Narendran S <122251349+SNarendranS@users.noreply.github.com>
2024-02-09 10:23:39 +0530

    Update commentService.js

--- news-portal-frontend/src/services/commentService.js
+++ news-portal-frontend/src/services/commentService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const comment = (id) => {
   try{
     const token = sessionStorage.getItem('token');
-    return axios.post('http://localhost:8080/comment',{id:id},{headers:{'Authorization': 'Bearer '+JSON.parse(token)}});
+    return axios.post('https://chronical-backend.onrender.com/comment',{id:id},{headers:{'Authorization': 'Bearer '+JSON.parse(token)}});
   }catch(error){
     console.log('cannot fetch' + error)
   }
@@ -12,7 +12,7 @@ const comment = (id) => {
 const createComment = (postComment) => {
   try{
     const token = sessionStorage.getItem('token');
-    return axios.post('http://localhost:8080/comment/post',postComment,{headers:{'Authorization': 'Bearer '+JSON.parse(token)}});
+    return axios.post('https://chronical-backend.onrender.com/comment/post',postComment,{headers:{'Authorization': 'Bearer '+JSON.parse(token)}});
   }catch{
     console.log('cannot fetch')
   }
